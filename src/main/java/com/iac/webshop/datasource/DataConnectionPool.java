91e5218a08c9293af988c19db492180bcaa90a75
MaxStolktirbig <max,stoltenborgh@student.hu.nl>
2020-04-01 13:47:15 +0200

    Test ready for db connection

--- src/main/java/com/iac/webshop/datasource/DataConnectionPool.java
+++ src/main/java/com/iac/webshop/datasource/DataConnectionPool.java
@@ -14,45 +14,26 @@ public class DataConnectionPool {
     private static List<Connection> openConnections;
     private static List<Connection> usedConnections = new ArrayList<>();
     private int INITIAL_POOL_SIZE = 10;
-    private String user ="root";
-    private String password = "rKxfpK57";
-    int lport = 5656;
-    String rhost = "85.214.169.10";
-    String host = "85.214.169.10";
     int rport = 3306;
     String dbuserName = "iac";
     String dbpassword = "I@c202020";
     String url = "jdbc:mysql://localhost:" + rport;
     String driverName = "com.mysql.cj.jdbc.Driver";
     Connection conn = null;
-    Session session = null;
 
     private DataConnectionPool(){
         if(INSTANCE == null) {
-            openConnections = createConnection(url, user, password);
+            openConnections = createConnection(url, dbuserName, dbpassword);
         }
     }
 
     private List<Connection> createConnection(String url, String user, String password) {
         List<Connection> pool = new ArrayList<>(INITIAL_POOL_SIZE);
         try {
-            //Set StrictHostKeyChecking property to no to avoid UnknownHostKey issue
-            java.util.Properties config = new java.util.Properties();
-            config.put("StrictHostKeyChecking", "no");
-            JSch jsch = new JSch();
-            session = jsch.getSession(user, host);
-            session.setPassword(password);
-            session.setConfig(config);
-            session.connect();
-            System.out.println("Connected to ssh");
-            int assinged_port = session.setPortForwardingL(lport, rhost, rport);
-            System.out.println("localhost:" + assinged_port + " -> " + rhost + ":" + rport);
-            System.out.println("Port Forwarded");
-
             //mysql database connectivity
             Class.forName(driverName);
             for(int i = 0; i< INITIAL_POOL_SIZE; i++) {
-                conn = DriverManager.getConnection(url, dbuserName, dbpassword);
+                conn = DriverManager.getConnection(url, user, password);
                 System.out.println("Database connection established (id = "+i+")");
                 System.out.println("[DONE]");
                 pool.add(conn);
